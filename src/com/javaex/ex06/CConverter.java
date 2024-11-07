f6716e5673d756ae1f8a73284b9bea1b6c0ec825
서민정 <ybsm0629@gmail.com>
2024-11-07 23:58:08 +0900

    연습

--- src/com/javaex/ex06/CConverter.java
+++ src/com/javaex/ex06/CConverter.java
@@ -6,6 +6,8 @@ public class CConverter {
     
     public static void setRate(double rate){
         CConverter.rate = rate;
+        // CConverter라는 class의 rate 변수가 무엇을 뜻하는지 알려줌
+        // 따라서 아래 method 에서부터는 CConverter라는 class의 rate 변수를 쓸 수 있게됨
     }
     
     public static double toDoller(double won){
