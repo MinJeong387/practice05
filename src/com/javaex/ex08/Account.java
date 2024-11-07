b562b49bcaccf50a21a7cc2ed31a9dd30383ef29
서민정 <ybsm0629@gmail.com>
2024-11-08 01:16:14 +0900

    연습 8 하는중... 어렵다 ㅜㅜ

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -2,11 +2,31 @@ package com.javaex.ex08;
 
 public class Account {
 
-    private String accountNo;
-    private int balance;
-    
-    //생성자 작성
-    
-    //필요한 메소드 작성
+	private String accountNo;
+	private int balance;
+
+	// 생성자 작성
+	public Account(String account) {
+
+	}
+
+	// 필요한 메소드 작성
+
+	public void deposit(int money) {
+		this.money=money;
+	}
+	public int deposit () {
+
+		return 
+	}
+
+	public int withdraw () {
+
+	}
+
+	public int showBalance () {
+		return 
+
+	}
 
 }
