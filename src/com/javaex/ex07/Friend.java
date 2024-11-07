5818d58b87d80de1f8e6306166aa71e8426b56d1
서민정 <ybsm0629@gmail.com>
2024-11-08 01:00:04 +0900

    연습 7까지 ok

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -2,17 +2,32 @@ package com.javaex.ex07;
 
 public class Friend {
 
-    private String name;
-    private String hp;
-    private String school;
+	private String name;
+	private String hp;
+	private String school;
 
-    
-    // getter/setter 작성
-    
-    
-    
-    public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
-    }
+	// getter/setter 작성
+
+	public Friend(String name, String hp, String school) {
+		this.name = name;
+		this.hp = hp;
+		this.school = school;
+	}
+/*
+	public String getName() {
+		return name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+*/
+	public void showInfo() {
+		System.out.println("이름:" + name + "  핸드폰:" + hp + "  학교:" + school);
+	}
 
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 public class FriendApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Friend[] friendArray = new Friend[3];
-        Scanner sc = new Scanner(System.in);
+		Friend[] friendArray = new Friend[3];
+		Scanner sc = new Scanner(System.in);
 
-        System.out.println("친구를 3명 등록해 주세요");
+		System.out.println("친구를 3명 등록해 주세요");
 
-        
-        //for문 시작
-        //친구정보 3명 입력 로직 --> 반복문 사용
-            
-      
-        //for문 끝
-        
-        
-        // 친구정보 출력
-        for (int i = 0; i < friendArray.length; i++) {
-            //친구정보 출력 메소드 호출
-        }
+		// for문 시작
+		// 친구정보 3명 입력 로직 --> 반복문 사용
+		for (int i = 0; i < friendArray.length; i++) {
+			System.out.print("이름: ");
+			String name = sc.next();
 
-        sc.close();
-    }
+			System.out.print("핸드폰: ");
+			String hp = sc.next();
+
+			System.out.print("학교: ");
+			String school = sc.next();
+
+			friendArray[i] = new Friend(name, hp, school);
+		}
+
+		// for문 끝
+
+		// 친구정보 출력
+		for (int i = 0; i < friendArray.length; i++) {
+			// 친구정보 출력 메소드 호출
+			friendArray[i].showInfo();
+		}
+
+		sc.close();
+	}
 
 }
